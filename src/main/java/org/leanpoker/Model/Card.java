2968cc27aad1bfc9016ef66760e452d3f6b85ab3
Zsolt Gajdács <33376024+ZsoltGajdacs@users.noreply.github.com>
2018-08-27 11:39:25 +0200

    Models created

---
+++ src/main/java/org/leanpoker/Model/Card.java
@@ -0,0 +1,27 @@
+package org.leanpoker.Model;
+
+public class Card {
+    String rank;
+    String suit;
+
+    public Card(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+}
